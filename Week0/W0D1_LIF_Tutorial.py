25eb406714fbcd1864f4a8379188fac88314ecf1
bosemessi <sb2293@cornell.edu>
2024-06-27 18:39:59 +0530

    LIF Tutorial 1 completed

--- Week0/W0D1_LIF_Tutorial.py
+++ Week0/W0D1_LIF_Tutorial.py
@@ -1,6 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+###############################################
+### Coding Exercise 1 : Defining parameters ###
+###############################################
+
+
 t_max = 150e-3   # second
 dt = 1e-3        # second
 tau = 20e-3      # second
@@ -10,22 +15,28 @@ vth = -50e-3     # milivolt
 r = 100e6        # ohm
 i_mean = 25e-11  # ampere
 
-# print(t_max, dt, tau, el, vr, vth, r, i_mean)
+print(t_max, dt, tau, el, vr, vth, r, i_mean)
 
-# t = np.arange(0, 0.010, 0.001)
-# I_t = i_mean * (1 + np.sin(2*np.pi*t/0.01))
+######################################################
+### Coding Exercise 2: Simulating an input current ###
+######################################################
 
-# for i in range(len(t)):
-#     print(f"{t[i]:.3f}  {I_t[i]:.4e}")
+t = np.arange(0, 0.010, 0.001)
+I_t = i_mean * (1 + np.sin(2*np.pi*t/0.01))
 
-# plt.plot(t, I_t)
-# plt.show()
+######################################################
+### Coding Exercise 3: Printing pretty numbers #######
+######################################################
 
-#################################################
-## TODO for students: fill out compute v code ##
-# Fill out code and comment or remove the next line
-# raise NotImplementedError("Student exercise: You need to fill out code to compute v")
-#################################################
+for i in range(len(t)):
+    print(f"{t[i]:.3f}  {I_t[i]:.4e}")
+
+plt.plot(t, I_t)
+plt.show()
+
+########################################################
+### Coding Exercise 4: Simulating membrane potential ###
+########################################################
 
 # Initialize step_end and v0
 
@@ -47,9 +58,7 @@ for step in range(step_end):
   print(f"{t:.3f} {v:.4e}")
 
 #################################################
-## TODO for students: fill out the figure initialization and plotting code below ##
-# Fill out code and comment or remove the next line
-# raise NotImplementedError("Student exercise: You need to fill out current figure code")
+### Coding Exercise 5: Plotting current #########
 #################################################
 
 # Initialize step_end
@@ -76,11 +85,9 @@ for step in range(step_end):
 # Display the plot
 plt.show()
 
-#################################################
-## TODO for students: fill out the figure initialization and plotting code below ##
-# Fill out code and comment or remove the next line
-# raise NotImplementedError("Student exercise: You need to fill out membrane potential figure code")
-#################################################
+######################################################
+### Coding Exercise 6: Plotting membrane potential ###
+######################################################
 
 # Initialize step_end
 step_end = int(t_max / dt)
@@ -112,11 +119,8 @@ for step in range(step_end):
 # Display plot
 plt.show()
 
-
 #################################################
-## TODO for students: fill out code to get random input ##
-# Fill out code and comment or remove the next line
-# raise NotImplementedError("Student exercise: You need to fill out random input code")
+### Coding Exercise 7: Adding randomness ########
 #################################################
 
 # Set random number generator
@@ -151,16 +155,296 @@ for step in range(step_end):
   # Plot v (using 'k.' to get even smaller markers)
   plt.plot(t, v, 'k.')
 
+# Display plot
+plt.show()
+
+#######################################################
+### Coding Exercise 8: Storing simulations in lists ###
+#######################################################
+
+# Set random number generator
+np.random.seed(2020)
+
+# Initialize step_end and n
+step_end = int(t_max / dt)
+n = 50
+
+# Intiatialize the list v_n with 50 values of membrane leak potential el
+v_n = [el] * n
+
+with plt.xkcd():
+  # Initialize the figure
+  plt.figure()
+  plt.title('Multiple realizations of $V_m$')
+  plt.xlabel('time (s)')
+  plt.ylabel('$V_m$ (V)')
+
+  # Loop for step_end steps
+  for step in range(step_end):
+
+    # Compute value of t
+    t = step * dt
+
+    # Loop for n simulations
+    for j in range(0, n):
+
+      # Compute value of i at this time step
+      i = i_mean * (1 + 0.1 * (t_max/dt)**(0.5) * (2* np.random.random() - 1))
+
+      # Compute value of v for this simulation
+      v_n[j] = v_n[j] + dt * (el - v_n[j] + r*i)/tau
+
+    # Plot all simulations (use alpha = 0.1 to make each marker slightly transparent)
+    plt.plot([t]*n, v_n, 'k.', alpha = 0.1)
+
+  # Display plot
+  plt.show()
+
+#################################################
+### Coding Exercise 9: Plotting sample mean #####
+#################################################
+
+# Set random number generator
+np.random.seed(2020)
+
+# Initialize step_end and n
+step_end = int(t_max / dt)
+n = 50
+
+# Intiatialize the list v_n with 50 values of membrane leak potential el
+v_n = [el] * n
+
+# Initialize the figure
+plt.figure()
+plt.title('Multiple realizations of $V_m$')
+plt.xlabel('time (s)')
+plt.ylabel('$V_m$ (V)')
+
+# Loop for step_end steps
+for step in range(step_end):
+
+  # Compute value of t
+  t = step * dt
+
+  # Loop for n simulations
+  for j in range(0, n):
+
+    # Compute value of i at this time step
+    i = i_mean * (1 + 0.1 * (t_max/dt)**(0.5) * (2* np.random.random() - 1))
+
+    # Compute value of v for this simulation
+    v_n[j] = v_n[j] + (dt / tau) * (el - v_n[j] + r*i)
+
+  # Compute sample mean by summing list of v_n using sum, and dividing by n
+  v_mean = sum(v_n)/n
+
+  # Plot simulations
+  plt.plot(n*[t], v_n, 'k.', alpha=0.1)
+
+  # Plot sample mean using alpha=0.8 and'C0.' for blue
+  plt.plot(t, v_mean, 'C0.', alpha=0.8)
 
 # Display plot
 plt.show()
 
+##############################################################
+### Coding Exercise 10: Plotting sample standard deviation ###
+##############################################################
+
+# Set random number generator
+np.random.seed(2020)
+
+# Initialize step_end and n
+step_end = int(t_max / dt)
+n = 50
+
+# Intiatialize the list v_n with 50 values of membrane leak potential el
+v_n = [el] * n
+
+# Initialize the figure
+plt.figure()
+plt.title('Multiple realizations of $V_m$')
+plt.xlabel('time (s)')
+plt.ylabel('$V_m$ (V)')
+
+# Loop for step_end steps
+for step in range(step_end):
+
+  # Compute value of t
+  t = step * dt
+
+  # Loop for n simulations
+  for j in range(0, n):
+
+    # Compute value of i at this time step
+    i = i_mean * (1 + 0.1 * (t_max/dt)**(0.5) * (2* np.random.random() - 1))
+
+    # Compute value of v for this simulation
+    v_n[j] = v_n[j] + (dt / tau) * (el - v_n[j] + r*i)
+
+  # Compute sample mean
+  v_mean = sum(v_n) / n
+
+  # Initialize a list `v_var_n` with the contribution of each V_n(t) to
+  # Var(t) with a list comprehension over values of v_n
+  v_var_n = [(v_n[j] - v_mean)**2 for j in range(n)]
+
+  # Compute sample variance v_var by summing the values of v_var_n with sum and dividing by n-1
+  v_var = sum(v_var_n)/(n-1)
+
+  # Compute the standard deviation v_std with the function np.sqrt
+  v_std = np.sqrt(v_var)
+
+  # Plot simulations
+  plt.plot(n*[t], v_n, 'k.', alpha=0.1)
+
+  # Plot sample mean using alpha=0.8 and'C0.' for blue
+  plt.plot(t, v_mean, 'C0.', alpha=0.8, markersize=10)
+
+  # Plot mean + standard deviation with alpha=0.8 and argument 'C7.'
+  plt.plot(t, v_mean + v_std, 'C7.', alpha=0.8)
+
+  # Plot mean - standard deviation with alpha=0.8 and argument 'C7.'
+  plt.plot(t, v_mean - v_std, 'C7.', alpha=0.8)
 
 
+# Display plot
+plt.show()
+
+#################################################
+### Coding Exercise 11: Rewriting with NumPy ####
+#################################################
+
+# Set random number generator
+np.random.seed(2020)
+
+# Initialize step_end, t_range, v
+step_end = int(t_max / dt) - 1
+t_range = np.linspace(0, t_max, num=step_end, endpoint=False)
+v = el * np.ones(step_end)
+
+# Simulate current over time
+i = i_mean * (1 + 0.1 * (t_max/dt)**(0.5) * (2* np.random.random(step_end) - 1))
+
+# Loop for step_end steps
+for step in range(1, step_end):
+
+  # Compute v as function of i
+  v[step] = v[step-1] + dt * (el - v[step-1] + r*i[step-1]) / tau
 
 
+# Plot membrane potential
+plt.figure()
+plt.title('$V_m$ with random I(t)')
+plt.xlabel('time (s)')
+plt.ylabel('$V_m$ (V)')
+
+plt.plot(t_range, v, 'k.')
+plt.show()
 
 
+######################################################
+### Coding Exercise 12: Using enumerate & indexing ###
+######################################################
 
+# Set random number generator
+np.random.seed(2020)
 
+# Initialize step_end, t_range, v
+step_end = int(t_max / dt) - 1
+t_range = np.linspace(0, t_max, num=step_end, endpoint=False)
+v = el * np.ones(step_end)
+
+# Simulate current over time
+i = i_mean * (1 + 0.1 * (t_max/dt) ** (0.5) * (2 * np.random.random(step_end) - 1))
+
+# Loop for step_end values of i using enumerate
+for step, i_step in enumerate(i):
+
+  # Skip first iteration
+  if step==0:
+    continue
+
+  # Compute v as function of i using i_step
+  v[step] = v[step-1] + dt * (el - v[step-1] + r*i[step-1])/tau
+
+# Plot figure
+plt.figure()
+plt.title('$V_m$ with random I(t)')
+plt.xlabel('time (s)')
+plt.ylabel('$V_m$ (V)')
+
+plt.plot(t_range, v, 'k')
+plt.show()
+
+#################################################
+### Coding Exercise 13: Using 2D arrays #########
+#################################################
+
+# Set random number generator
+np.random.seed(2020)
+
+# Initialize step_end, n, t_range, v and i
+step_end = int(t_max / dt)
+n = 50
+t_range = np.linspace(0, t_max, num=step_end)
+v_n = el * np.ones([n, step_end])
+i = i_mean * (1 + 0.1 * (t_max/dt) ** (0.5) * (2 * np.random.random((n, step_end)) - 1))
+
+# Loop for step_end - 1 steps
+for step in range(1, step_end):
+
+   # Compute v_n
+   v_n[:, step] = v_n[:, step-1] + dt * (el - v_n[:, step-1] + r*i[:, step-1])/tau
+
+# Plot figure
+plt.figure()
+plt.title('Multiple realizations of $V_m$')
+plt.xlabel('time (s)')
+plt.ylabel('$V_m$ (V)')
+
+plt.plot(t_range, v_n.T, "k", alpha=0.3)
+plt.show()
+
+#######################################################################
+### Coding Exercise 14: Plotting sample mean and standard deviation ###
+#######################################################################
+
+# Set random number generator
+np.random.seed(2020)
+
+# Initialize step_end, n, t_range, v and i
+step_end = int(t_max / dt)
+n = 50
+t_range = np.linspace(0, t_max, num=step_end)
+v_n = el * np.ones([n, step_end])
+i = i_mean * (1 + 0.1 * (t_max / dt)**(0.5) * (2 * np.random.random([n, step_end]) - 1))
+
+# Loop for step_end - 1 steps
+for step in range(1, step_end):
+
+  # Compute v_n
+  v_n[:, step] = v_n[:, step - 1] + (dt / tau) * (el - v_n[:, step - 1] + r * i[:, step])
+
+# Compute sample mean (use np.mean)
+v_mean = np.mean(v_n, axis = 0)
+
+# Compute sample standard deviation (use np.std)
+v_std = np.std(v_n, axis = 0)
+
+# Plot figure
+plt.figure()
+plt.title('Multiple realizations of $V_m$')
+plt.xlabel('time (s)')
+plt.ylabel('$V_m$ (V)')
+
+plt.plot(t_range, v_n.T, 'k', alpha=0.3)
+
+plt.plot(t_range, v_n[-1], 'k', alpha=0.3, label='V(t)')
+plt.plot(t_range, v_mean, 'C0', alpha=0.8, label='mean')
+plt.plot(t_range, v_mean+v_std, 'C7', alpha=0.8)
+plt.plot(t_range, v_mean-v_std, 'C7', alpha=0.8, label='mean $\pm$ std')
+
+plt.legend()
+plt.show()
 
